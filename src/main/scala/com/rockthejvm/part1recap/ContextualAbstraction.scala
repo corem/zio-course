0e1b63af4df011c11ffb1710c4eeaf8be71017ce
core <cornet.remi@gmail.com>
2023-10-31 22:34:25 -0400

    Contextual Abstraction

---
+++ src/main/scala/com/rockthejvm/part1recap/ContextualAbstraction.scala
@@ -0,0 +1,61 @@
+package com.rockthejvm.part1recap
+
+object ContextualAbstraction {
+
+  // given/using
+  def increment(x: Int)(using amount: Int): Int = x + amount
+  given defaultAmount: Int = 10
+  val twelve = increment(2)
+
+  def multiply(x: Int)(using factor: Int): Int = x * factor
+  val aHundred = multiply(10)
+
+  // Complex use case
+  trait Combiner[A] {
+    def combine(x: A, y: A): A
+    def empty: A
+  }
+
+  def combineAll[A](values: List[A])(using combiner: Combiner[A]): A =
+    values.foldLeft(combiner.empty)(combiner.combine)
+
+  given intCombiner: Combiner[Int] with {
+    override def combine(x: Int, y: Int): Int = x + y
+    override def empty = 0
+  }
+
+  val numbers = (1 to 10).toList
+  val sum10 = combineAll(numbers)
+
+  // Synthesize given instances
+  given optionCombiner[T](using combiner: Combiner[T]): Combiner[Option[T]] with {
+    override def empty: Option[T] = Some(combiner.empty)
+    override def combine(x: Option[T], y: Option[T]): Option[T] = for {
+      vx <- x
+      vy <- y
+    } yield combiner.combine(vx, vy)
+  }
+
+  val sumOptions: Option[Int] = combineAll(List(Some(1), None, Some(2)))
+
+  // Extension methods
+  case class Person(name: String) {
+    def greet(): String = s"Hi, my name is $name"
+  }
+
+  extension (name: String)
+    def greet(): String = Person(name).greet()
+
+  val aliceGreeting = "Alice".greet()
+
+  // Generic extension
+  extension [T](list: List[T])
+    def reduceAll(using combiner: Combiner[T]): T =
+      list.foldLeft(combiner.empty)(combiner.combine)
+
+  val sum10_v2 = numbers.reduceAll
+
+  // Type Classes Pattern
+
+  def main(array: Array[String]): Unit = {}
+}
