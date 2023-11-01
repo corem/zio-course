579cce7d7935ecf7a8cdc1a85adaa4e47c05b577
core <cornet.remi@gmail.com>
2023-10-31 21:42:06 -0400

    Variance

--- src/main/scala/com/rockthejvm/part1recap/Variance.scala
+++ src/main/scala/com/rockthejvm/part1recap/Variance.scala
@@ -5,10 +5,38 @@ object Variance {
   class Animal
   class Dog(name: String) extends Animal
 
-  // Variance question for List
+  // Covariant
+  val lassie = Dog("Lassie")
+  val hachi = Dog("Hachi")
+  val laika = Dog("Laika")
 
-  val lassie = Dog("lassie")
   val anAnimal: Animal = lassie
+  val someAnimal: List[Animal] = List(lassie, hachi, laika)
+
+  class MyList[+A]
+  val myAnimalList: MyList[Animal] = new MyList[Dog]
+
+  // Invariant
+  trait Semigroup[A] {
+    def combine(x: A, y: A): A
+  }
+
+  // all generics in Java
+  // val aJavaList: java.util.ArrayList[Animal] = new util.ArrayList[Dog]()
+
+  // Contravariance
+  trait Vet[-A] {
+    def heal(animal: A): Boolean
+  }
+
+  val myVet: Vet[Dog] = new Vet[Animal] {
+    override def heal(animal: Animal): Boolean = {
+      println("Here you go, you're good now...")
+      true
+    }
+  }
+
+  val healingLassie = myVet.heal(lassie)
 
   def main(array: Array[String]): Unit = {}
 }
