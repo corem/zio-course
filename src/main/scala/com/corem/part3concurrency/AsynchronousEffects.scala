6925e3f663279cd5c1455903515d9369e418a379
Remi Cornet <remi.c@campus.coach>
2023-11-09 16:30:15 -0500

    ZIO AsynchronousEffects

--- src/main/scala/com/corem/part3concurrency/AsynchronousEffects.scala
+++ src/main/scala/com/corem/part3concurrency/AsynchronousEffects.scala
@@ -1,10 +1,13 @@
 package com.corem.part3concurrency
 
-import com.corem.part3concurrency.AsynchronousEffects.LoginService.{AuthError, UserProfile}
+import com.corem.part3concurrency.AsynchronousEffects.LoginService.{
+  AuthError,
+  UserProfile
+}
 import zio.*
 import com.corem.utils.*
 
-import java.util.concurrent.Executors
+import java.util.concurrent.{ExecutorService, Executors}
 
 object AsynchronousEffects extends ZIOAppDefault {
 
@@ -27,25 +30,57 @@ object AsynchronousEffects extends ZIOAppDefault {
       "daniel@rockthejvm.com" -> "Daniel"
     )
 
-    def login(email: String, password: String)(onSuccess: UserProfile => Unit, onFailure: AuthError => Unit) =
+    def login(
+        email: String,
+        password: String
+    )(onSuccess: UserProfile => Unit, onFailure: AuthError => Unit) =
       executor.execute { () =>
-        println(s"[${Thread.currentThread().getName}] Attempting login for $email")
+        println(
+          s"[${Thread.currentThread().getName}] Attempting login for $email"
+        )
         passwd.get(email) match {
           case Some(`password`) =>
             onSuccess(UserProfile(email, database(email)))
           case Some(_) => onFailure(AuthError("Incorrect password"))
-          case None => onFailure(AuthError(s"User $email doesn't exist. Please sign up."))
+          case None =>
+            onFailure(AuthError(s"User $email doesn't exist. Please sign up."))
         }
       }
   }
 
-  def loginAsZIO(id: String, pw: String): ZIO[Any, LoginService.AuthError, LoginService.UserProfile] =
-    ZIO.async[Any, LoginService.AuthError, LoginService.UserProfile] { cb => // callback object created by ZIO
-      LoginService.login(id, pw)(
-        profile => cb(ZIO.succeed(profile)),
-        error => cb(ZIO.fail(error))
-      )
+  def loginAsZIO(
+      id: String,
+      pw: String
+  ): ZIO[Any, LoginService.AuthError, LoginService.UserProfile] =
+    ZIO.async[Any, LoginService.AuthError, LoginService.UserProfile] {
+      cb => // callback object created by ZIO
+        LoginService.login(id, pw)(
+          profile => cb(ZIO.succeed(profile)),
+          error => cb(ZIO.fail(error))
+        )
     }
 
-  def run = ???
+  val loginProgram = for {
+    email <- Console.readLine("Email: ")
+    pass <- Console.readLine("Password: ")
+    profile <- loginAsZIO(email, pass).debugThread
+    _ <- Console
+      .printLine(s"Welcome to Rock the JVM, ${profile.name}")
+  } yield ()
+
+  /** Exercices
+    */
+
+  // 1. Surface a computation running on some (external) thread to a ZIO
+  def external2ZIO[A](computation: () => A)(
+      executor: ExecutorService
+  ): Task[A] = ???
+
+  val demoExternal2ZIO = {
+    val zio = external2ZIO { () =>
+      println(s"")
+    }
+  }
+
+  def run = loginProgram
 }
