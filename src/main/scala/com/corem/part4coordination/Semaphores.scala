60f61bed4af1cbfbe5110c46de6526ee6b261185
core <cornet.remi@gmail.com>
2023-11-11 19:11:55 -0500

    ZIO Semaphores

---
+++ src/main/scala/com/corem/part4coordination/Semaphores.scala
@@ -0,0 +1,94 @@
+package com.corem.part4coordination
+
+import zio._
+import com.corem.utils._
+
+object Semaphores extends ZIOAppDefault {
+  val aSemaphore = Semaphore.make(10)
+
+  // Example: limit the number of concurrent sessions on a server
+  def doWorkWhileLoggedIn(): UIO[Int] =
+    ZIO.sleep(1.second) *> Random.nextIntBounded(100)
+
+  def login(id: Int, sem: Semaphore): UIO[Int] =
+    ZIO.succeed(s"[Task $id] waiting to log in").debugThread *>
+      sem.withPermit {
+        for {
+          // Start - critical section
+          _ <- ZIO.succeed(s"[Task $id] logged in, working...").debugThread
+          res <- doWorkWhileLoggedIn()
+          _ <- ZIO.succeed(s"[Task $id] done: $res").debugThread
+        } yield res
+      }
+
+  def demoSemaphore() = for {
+    sem <- Semaphore.make(2) // Semaphore.make(1) == a Mutex
+    f1 <- login(1, sem).fork
+    f2 <- login(2, sem).fork
+    f3 <- login(3, sem).fork
+    _ <- f1.join
+    _ <- f2.join
+    _ <- f3.join
+  } yield ()
+
+  def loginWeighted(n: Int, sem: Semaphore): UIO[Int] =
+    ZIO.succeed(s"[Task $n] waiting to log in with $n permits").debugThread *>
+      sem.withPermits(n) { // acquire + zio + release
+        for {
+          // critical section starts when you acquired ALL n permits
+          _ <- ZIO.succeed(s"[Task $n] logged in, working...").debugThread
+          res <- doWorkWhileLoggedIn()
+          _ <- ZIO.succeed(s"[Task $n] done: $res").debugThread
+        } yield res
+      }
+
+  def demoSemaphoreWeighed() = for {
+    sem <- Semaphore.make(2)
+    f1 <- loginWeighted(1, sem).fork
+    f2 <- loginWeighted(2, sem).fork
+    f3 <- loginWeighted(3, sem).fork
+    _ <- f1.join
+    _ <- f2.join
+    _ <- f3.join
+  } yield ()
+
+  /**
+   * Exercise
+   * 1. What is the code supposed to do?
+   * 2. Find if there's anything wrong
+   * 3. Fix the problem
+   */
+  val mySemaphore = Semaphore.make(1)
+  val tasks = ZIO.collectAllPar((1 to 10).map { id =>
+    for {
+      sem <- mySemaphore
+      _ <- ZIO.succeed(s"[Task $id] waiting to log in").debugThread
+      res <- sem.withPermit {
+        for {
+          // Start - critical section
+          _ <- ZIO.succeed(s"[Task $id] logged in, working...").debugThread
+          res <- doWorkWhileLoggedIn()
+          _ <- ZIO.succeed(s"[Task $id] done: $res").debugThread
+        } yield res
+      }
+    } yield res
+  })
+
+  val tasksFixed = mySemaphore.flatMap { sem =>
+    ZIO.collectAllPar((1 to 10).map { id =>
+      for {
+        _ <- ZIO.succeed(s"[Task $id] waiting to log in").debugThread
+        res <- sem.withPermit {
+          for {
+            // Start - critical section
+            _ <- ZIO.succeed(s"[Task $id] logged in, working...").debugThread
+            res <- doWorkWhileLoggedIn()
+            _ <- ZIO.succeed(s"[Task $id] done: $res").debugThread
+          } yield res
+        }
+      } yield res
+    })
+  }
+
+  override def run = tasksFixed.debugThread
+}
